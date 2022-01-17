7d56abde7ff8fd9e8251c7b8ba28a91445886bd9
Jacob Stephens <jacob@stewardgoods.com>
2022-01-17 17:40:52 -0500

    add page-our-team.php

---
+++ brandywine-oak/page-our-team.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the our team page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Wyeth
+ */
+
+  get_header();
+  while ( have_posts() ) : the_post();
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <?php include(locate_template('template-parts/shared/banner.php', false, false )); ?>
+  <div class="entry-content">
+    <?php
+      if( have_rows('page_content') ) {
+        while ( have_rows('page_content') ) {
+          the_row();
+          include(locate_template( 'template-parts/flexible/flexible-'.get_row_layout().'.php', false, false ));
+        }
+      }
+    ?>
+  </div>
+</article><!-- #post-<?php the_ID(); ?> -->
+
+<?php
+  endwhile; // End of the loop.
+  get_footer();
